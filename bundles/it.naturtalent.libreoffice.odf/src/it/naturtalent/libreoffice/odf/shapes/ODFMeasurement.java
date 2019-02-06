e48442f092d0bff9c8c65b33e1fe29e3e8145d1a
dieter <dieter@dieter-VPCF13E8E>
2019-02-06 21:55:59 +0100

    import org.apache.commons.lang3

--- bundles/it.naturtalent.libreoffice.odf/src/it/naturtalent/libreoffice/odf/shapes/ODFMeasurement.java
+++ bundles/it.naturtalent.libreoffice.odf/src/it/naturtalent/libreoffice/odf/shapes/ODFMeasurement.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 public class ODFMeasurement
 {
